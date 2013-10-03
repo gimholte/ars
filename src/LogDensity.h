72bdc06c611539de55829c907a1456e9c50df8ed
Gregory Imholte <gimholte@gmail.com>
2013-10-03 06:58:23 -0700

    Fix weibull log derivative.

--- src/LogDensity.h
+++ src/LogDensity.h
@@ -26,11 +26,27 @@ public:
         rate = args[1];
     };
     double pdf(const double x) {
-        return (shape - 1.0) * log(x) - x * rate;
+        return (shape - (double) 1.0) * log(x) - x * rate;
     };
     double pdfDeriv(const double x) {
-        return (shape - 1.0) / x - rate;
+        return (shape - (double) 1.0) / x - rate;
     };
+};
+
+class WeibullDistribution : public LogDensity {
+    double k, ell;
+public:
+    void setParameters(double const * const args) {
+        k = args[0];
+        ell = args[1];
+    }
+    double pdf(const double x) {
+        return (k - (double) 1.0) * log(x) - pow(x / ell, k);
+    };
+    double pdfDeriv(const double x) {
+        return (k - (double) 1.0) / x - k * pow(x / ell, k - (double) 1.0) / ell;
+    }
+
 };
 
 
